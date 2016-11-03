12ec34b7eac6f950762a305678825fecfde4ea5d
ShiWenChen <cswzmg@live.com>
2016-11-03 11:07:45 +0800

    Initial Commit

---
+++ Post/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Post
+//
+//  Created by admin on 2016/11/3.
+//  Copyright © 2016年 Post. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Post/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Post
+//
+//  Created by admin on 2016/11/3.
+//  Copyright © 2016年 Post. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
